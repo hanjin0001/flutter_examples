4afbf4924459bfe3a259c378cbb61f64e171b466
hanjin <hanjin0001@gmail.com>
2021-07-12 11:37:24 +0900

    add text

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     "";
     "";
+    "new feature";
     return MaterialApp(
       title: 'Flutter Examples',
       theme: ThemeData(
